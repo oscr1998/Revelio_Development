fc6bea30431e115adda9d8d31c04e2feece0bcc5
Matthieu Chan <matthieuchan96@gmail.com>
2022-12-05 19:01:37 +0100

    bit of styling

--- client/src/pages/Menu/index.js
+++ client/src/pages/Menu/index.js
@@ -2,26 +2,28 @@ import { React, useState } from 'react'
 import { Login, Register } from '../../components'
 import { Link } from 'react-router-dom';
 import './style.css'
-
+import Zoom from 'react-reveal/Zoom';
 
 export default function Menu() {
 
     const [ regModel, setRegModel ] = useState(false)
 
     return (
-        <div className='loginContainer nes-container is-centered'>
-            <h1>Logo</h1>
-
+        <div id='menuContainer'>
+        <h1>Logo</h1>
+        <Zoom><div className='loginContainer nes-container is-centered'>
+            
             <Login />
 
             <button onClick={() => setRegModel(true)} className="nes-btn is-primary">Register</button>
             {regModel? <Register  setRegModel={setRegModel}/> : null}
-
             
             <button className="nes-btn">
                 <Link to='/leaderboard'>Leaderboard</Link>
             </button>
             
+        </div>
+        </Zoom>
         </div>
     )
 }
