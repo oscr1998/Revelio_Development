3a6a445149ad7ba8e01ed86da1e05553c84b6c0d
Matthieu Chan <matthieuchan96@gmail.com>
2022-12-08 02:22:01 +0100

    empty

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -1,18 +1,45 @@
-import React, { useEffect, useState } from 'react'
-import Backdrop from '../Backdrop'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { motion } from "framer-motion";
+import Backdrop from "../Backdrop";
+import './style.css'
 
+const dropIn = {
+  hidden: {
+    y: "-100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: "0",
+    opacity: 1,
+    transition: {
+      duration: 0.1,
+      type: "spring",
+      damping: 25,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: "100vh",
+    opacity: 0,
+  },
+};
 
-
-export default function Results({handleClose, results}) {
-
-    const navigate = useNavigate()
-
-    return (
-        <Backdrop onClick={handleClose}>
-            <h1>Results</h1>
-            <h3>{results} Side Win!</h3>
-            <button onClick={()=>{ navigate('/dashboard') }}>Back to Home</button>
-        </Backdrop>
-    )
+export default function Results({ handleClose }) {
+  return (
+    <Backdrop onClick={handleClose}>
+      <motion.div
+        onClick={(e) => e.stopPropagation()}
+        variants={dropIn}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        className="resultscontainer"
+      >
+        <button className="closebtn" onClick={handleClose}>
+          x
+        </button>
+        <h1>Results</h1>
+      </motion.div>
+    </Backdrop>
+  );
 }
